0adaa6eea74888ff8abea83615009845aa67a72d
regruss <44583658+regruss@users.noreply.github.com>
2023-09-18 12:31:06 -0500

    Add files via upload

---
+++ energy_home.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 10 20:38:01 2023
+
+@author: regru
+"""
+import os
+import json
+import numpy as np
+import pandas as pd
+import time
+import spacy
+import openai
+import glob
+import re
+import pytesseract
+from pdf2image import convert_from_path
+import PyPDF2
+import io
+from PyPDF2 import PdfReader
+from IPython.display import display
+import streamlit as st
+
+
+
+# https://streamlit.io/gallery
+# Find emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+def main():
+    st.set_page_config(page_title="Contract Data Extraction",page_icon=":house:")
+    st.header("Extract Data From Your Contracts")
+    pdf_files = st.file_uploader("Upload Files", accept_multiple_files=True, type="pdf")
+    # Extract Data
+    if bool(pdf_files):
+        st.header('Contract Data Preview')
+        # Insert into DF
+        contract_df = pd.read_csv(r'C:\Users\regru\Desktop\PDF_OCR\Energy_Contracts\Dashboard\energy_contract_dash_df.csv')
+        st.write(contract_df)
+if __name__== '__main__':
+    main()
